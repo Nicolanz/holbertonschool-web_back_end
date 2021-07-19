5569515e77ac98c24e081a2c629816102672965a
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-19 19:10:07 +0000

    2-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to floor of floats"""
+
+
+def floor(n: float) -> int:
+    """Function to floor floats
+
+    Args:
+        n (float): [num]
+
+    Returns:
+        int: [floor of n]
+    """
+    return int(n)
