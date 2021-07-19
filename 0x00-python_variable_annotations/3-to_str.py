a6fe248b98b5a0b7b568406e45f81d11e50422bd
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-19 19:17:13 +0000

    3-to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to return str repr of floats"""
+
+
+def to_str(n: float) -> str:
+    """Function to get the string representation of floats
+
+    Args:
+        n (float): [float number]
+
+    Returns:
+        str: [str repr of n]
+    """
+    return str(n)
