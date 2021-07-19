0432f3075f06dc78e87f7a0fd15494d18c7d7c40
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-19 18:28:01 +0000

    0-add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to resolve the first task"""
+
+
+def add(a: float, b: float) -> float:
+    """Function to sum floats
+
+    Args:
+        a (float): [first float]
+        b (float): [second float]
+
+    Returns:
+        float: [returns the a + b function]
+    """
+    return a + b
