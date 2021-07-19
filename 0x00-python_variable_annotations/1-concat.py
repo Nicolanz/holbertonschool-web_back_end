bf99528460c1fb8e4e2689b2074f8ac0eca067c0
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-19 19:03:16 +0000

    1-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to concat strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concat strings
+
+    Args:
+        str1 (str): [First string]
+        str2 (str): [Second string]
+
+    Returns:
+        str: [Concatenated string]
+    """
+    str3 = str1 + str2
+    return str3
