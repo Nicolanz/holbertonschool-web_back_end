edc884d0642e177cbfdbe9bee7ccf949ccb1d5e9
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-21 20:07:15 +0000

    Task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module to understand the async basis"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """returns a rand after waiting some time
+
+    Args:
+        max_delay (int, optional): [max number of the rand]. Defaults to 10.
+
+    Returns:
+        [float]: [Number between the rand 0, max_delay]
+    """
+    num = random.uniform(0, max_delay)
+    await asyncio.sleep(num)
+    return num
