fb28c6379503a3271ede4cebe921acf01e33bde4
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-21 23:33:14 +0000

    Task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,19 +3,18 @@
 
 
 import asyncio
-from asyncio.tasks import Task
 from typing import Coroutine
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Coroutine[Task]:
+def task_wait_random(max_delay: int) -> Coroutine:
     """Function to return new task
 
     Args:
         max_delay (int): [max num for the range]
 
     Returns:
-        Coroutine: [new task object]
+        Coroutine: [new task]
     """
     new_task = asyncio.create_task(wait_random(max_delay))
     return new_task
