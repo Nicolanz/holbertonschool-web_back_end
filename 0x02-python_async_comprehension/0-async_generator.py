c4c1b1905905ef4f3f2df42f88a6f6e075931ea1
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-22 00:25:44 +0000

    Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module containing async generator"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Async generator
+
+    Yields:
+        [type]: [yields numbers]
+    """
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
