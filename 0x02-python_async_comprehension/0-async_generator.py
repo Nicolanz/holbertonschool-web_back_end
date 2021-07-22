69cb56e310ecb6368fc4a4badda65552ffec322c
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-22 00:35:04 +0000

    Task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,4 +13,5 @@ async def async_generator():
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        num = random.uniform(0, 10)
+        yield num
