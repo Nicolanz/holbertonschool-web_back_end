a79a1ce8a65d0f32786c093c1de25d4745f05210
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-27 23:31:31 +0000

    Task 0

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Module containing index_range function"""
-from typing import Tuple, Union
 
 
-def index_range(page: int, page_size: int) -> Tuple[Union[int, int]]:
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Index range function
 
     Args:
@@ -13,6 +15,6 @@ def index_range(page: int, page_size: int) -> Tuple[Union[int, int]]:
     Returns:
         Tuple[int, int]: [Tuple with two values]
     """
-    my_page = 15 * (page - 1)
-    my_page_size = my_page + page_size
-    return((my_page, my_page_size))
+    my_page = page_size * (page - 1)
+    my_page_size = page * page_size
+    return my_page, my_page_size
