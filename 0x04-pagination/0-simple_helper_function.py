5a6acd693cc4780b741a7c63094086767efb2b7d
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-07-27 22:38:11 +0000

    0-task

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module containing index_range function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Index range function
+
+    Args:
+        page (int): [Number of page]
+        page_size (int): [Number of page size]
+
+    Returns:
+        Tuple[int, int]: [Tuple with two values]
+    """
+    my_page = 15 * (page - 1)
+    my_page_size = my_page + page_size
+    return((my_page, my_page_size))
