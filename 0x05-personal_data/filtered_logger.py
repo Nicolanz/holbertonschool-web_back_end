da4edc8583e9c03d097934f65057f7ebf9551df6
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-02 20:58:28 +0000

    Annotate task

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """Filter module"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+                 redaction: str, message: str,
+                 separator: str) -> str:
     """Filter funtion"""
     new_str = re.sub(fields[0] + '=[a-z]*' + separator, fields[0] + "=" +
-                         redaction + separator, message)
+                     redaction + separator, message)
     new_str = re.sub(fields[1] + '=' + '.*' + separator, fields[1] +
                      "=" + redaction + separator, new_str)
     return new_str
