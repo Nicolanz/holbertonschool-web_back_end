f3c1a41dcee435884046d00e3cb1c6c373507f31
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-02 21:12:44 +0000

    filter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,9 +7,8 @@ from typing import List
 def filter_datum(fields: List[str],
                  redaction: str, message: str,
                  separator: str) -> str:
-    """Filter funtion"""
+    """Filter function"""
     new_str = re.sub(fields[0] + '=[a-z]*' + separator, fields[0] + "=" +
                      redaction + separator, message)
-    new_str = re.sub(fields[1] + '=' + '.*' + separator, fields[1] +
-                     "=" + redaction + separator, new_str)
-    return new_str
+    return re.sub(fields[1] + '=' + '.*' + separator, fields[1] +
+                  "=" + redaction + separator, new_str)
