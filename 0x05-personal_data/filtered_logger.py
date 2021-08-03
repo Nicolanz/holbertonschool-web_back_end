4b1bc0b2897aa27934167b8b61f80eac6a342ca8
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-03 18:45:43 +0000

    Filtered

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -18,7 +18,7 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    def format(self, record: logging.LogRecord):
+    def format(self, record: logging.LogRecord) -> str:
         """Format method"""
         logging.basicConfig(level=record.levelname,
                             format=RedactingFormatter.FORMAT)
