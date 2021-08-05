6a899e81a282f4c337ef1174853d68030cea722f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-05 20:23:28 +0000

    Path auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,7 @@ class Auth:
             return True
         if len(excluded_paths) == 0 or excluded_paths is None:
             return True
-        if path[-1] != '/':
+        if path[-1:] != '/':
             new_path = path + '/'
         else:
             new_path = path
