66e31b17744b31a82493cde40af41b3a1c3d920e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-05 23:16:31 +0000

    Basic Auth

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing the BasicAuth class that inherirs from Auth class"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth class"""
+    pass
