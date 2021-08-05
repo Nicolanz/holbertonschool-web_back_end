99789edd6e8c0274e5a26e3833bce4c271a271ce
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-05 01:55:33 +0000

    Check encrypt password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -10,3 +10,12 @@ def hash_password(password: str) -> bytes:
     password = password.encode()
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid function"""
+    check = password.encode()
+    if bcrypt.checkpw(check, hashed_password):
+        return True
+    else:
+        return False
