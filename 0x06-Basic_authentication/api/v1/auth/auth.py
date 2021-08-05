a406f4eaf8e90da8787dc14e0bbe3c915f0d4891
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-05 19:40:57 +0000

    Require auth function

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,19 @@ class Auth:
     """Auth Main Class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Function to require auth"""
-        return False
+        if path is None:
+            return True
+        if len(excluded_paths) <= 0 or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            new_path = path + '/'
+        else:
+            new_path = path
+
+        if new_path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """function to get the auth header"""
