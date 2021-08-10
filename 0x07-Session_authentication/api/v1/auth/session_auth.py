277d7e47ef41f811e9aa3923503629462cfb1655
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-10 02:51:43 +0000

    Create Session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """Module containing the SessionAuth subclass"""
+import uuid
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth subclass"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create session method"""
+        if user_id is None or type(user_id) != str:
+            return None
+        id = uuid.uuid4()
+        SessionAuth.user_id_by_session_id[str(id)] = user_id
+        return id
