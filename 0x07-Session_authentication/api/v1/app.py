2f6603a7334e4769d797b2bc40f00ebfec1bad5f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-10 23:42:16 +0000

    Auth before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -57,11 +57,14 @@ def before_request():
         paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         tmp = auth.require_auth(request.path, paths)
         if tmp is True:
-            if auth.authorization_header(request) is None:
+
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
                 return None
 
