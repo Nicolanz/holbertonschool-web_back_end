7a7d80410f77becb21163f53a3b2edad22641f32
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-17 17:30:18 +0000

    DB

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,7 +4,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
@@ -33,8 +32,6 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Method to add a new user"""
         newUser = User(email=email, hashed_password=hashed_password)
-
         self._session.add(newUser)
         self._session.commit()
-
         return(newUser)
