a0c566fb07db8cf4b5800c1be055c1ad02be0885
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 02:06:06 +0000

    Annotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import Dict
 
 from user import Base, User
 
@@ -39,12 +40,12 @@ class DB:
         self._session.commit()
         return(newUser)
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs: Dict) -> User:
         """Method to find a user"""
         try:
             instance = self._session.query(User).filter_by(**kwargs)[0]
             return instance
         except InvalidRequestError:
-            raise InvalidRequestError()
+            raise InvalidRequestError
         except IndexError:
-            raise NoResultFound()
+            raise NoResultFound
