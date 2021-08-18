fbe86ac82f90e8aa05637050134ae6337477a959
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 22:41:36 +0000

    Destror a session

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -57,6 +57,14 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """Method to destroy a session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            pass
+        return None
+
 
 def _hash_password(password: str) -> bytes:
     """Creates a hash password"""
