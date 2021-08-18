046a9ad72f4d5c5d540cb70dd6115d772622cfe3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 05:16:31 +0000

    Register new user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """Route module for the Flask app"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +16,22 @@ def bienvenue() -> str:
       - Bienvenue message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """Function to register a user from the Flask app"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify(
+            {"email": "{}".format(email), "message": "user created"}
+        )
+
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
