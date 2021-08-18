58685e438400dbef60978440434edef50b00547d
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 05:36:57 +0000

    Valid user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -24,6 +24,17 @@ class Auth:
             user = self._db.add_user(email, my_password)
             return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Method to check credential validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(bytes(password, 'utf-8'), user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Creates a hash password"""
