1d37751f671cef8c883710229cf712bbc9d8ac7e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 22:21:53 +0000

    Union

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -47,16 +47,16 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Method to get a user from a session id"""
         if not session_id:
             return None
         try:
-            user = self._db.find_user_by(session_id=session_id)
+            my_user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
 
-        return user
+        return my_user
 
 
 def _hash_password(password: str) -> bytes:
