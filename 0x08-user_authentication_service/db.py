20944a237f763f02a12eb7b9c9584d246aeab9e0
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 03:17:46 +0000

    Filter

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,6 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,5 +39,10 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Method to find a user"""
-        instance = self._session.query(User).filter_by(**kwargs).one()
-        return instance
+        try:
+            instance = self._session.query(User).filter_by(**kwargs)[0]
+            return instance
+        except InvalidRequestError:
+            raise InvalidRequestError
+        except IndexError:
+            raise NoResultFound
