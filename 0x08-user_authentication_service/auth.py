c8289deb4764c7e3356b6a69004a0c4cf41ebcd6
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 06:34:12 +0000

    Try session

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -40,11 +40,11 @@ class Auth:
         """Method to assign a unique id"""
         try:
             user = self._db.find_user_by(email=email)
-            u_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=u_id)
-            return u_id
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
-            return
+            return None
 
 
 def _hash_password(password: str) -> bytes:
