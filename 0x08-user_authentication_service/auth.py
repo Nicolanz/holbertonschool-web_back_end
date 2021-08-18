954825f36a79b7f50fef24f37b48565209df8c0b
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 04:09:12 +0000

    Hash password

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Auth module"""
+
+import bcrypt
+
+def _hash_password(password:str) -> bytes:
+    """Creates a hash password"""
+    new_password = bytes(password, 'utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(new_password, salt)
+
+    return hashed
