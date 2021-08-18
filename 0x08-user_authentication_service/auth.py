8168de2c0dad1396f1d56f42087b2dd1554e1dde
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-18 06:03:22 +0000

    session id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -36,6 +36,15 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        try:
+            user = self._db.find_user_by(email=email)
+            u_id = _generate_uuid()
+            self._db.update_user(user.id, id=u_id)
+            return u_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Creates a hash password"""
