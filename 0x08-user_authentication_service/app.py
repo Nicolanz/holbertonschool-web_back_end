02976c9706a0c8d33175cac7600c2bb60ba7dd63
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-19 04:39:07 +0000

    Last task

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -82,7 +82,7 @@ def get_reset_password_token():
     """Resets a token"""
     email = request.form.get("email")
     if not email:
-        return abort(403)
+        abort(403)
 
     token = AUTH.get_reset_password_token(email)
 
@@ -90,6 +90,21 @@ def get_reset_password_token():
         {"email": "{}".format(email), "reset_token": "{}".format(token)}
     ), 200
 
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """Updates password"""
+    email = request.form.get("email")
+    new_password = request.form.get("new_password")
+    reset_token = request.form.get("reset_token")
+    try:
+        AUTH._db.find_user_by(email=email)
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({
+            "email": "{}".format(email),
+            "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
