6d9740ac44dedd7a61e044a6bf6c44daf905b27b
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-19 04:18:43 +0000

    Upadte

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -76,6 +76,20 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a password"""
+        try:
+            new_user = self._db.find_user_by(reset_token=reset_token)
+            hashed = _hash_password(password)
+            self.db.update_user(
+                user.id,
+                hashed_password=hashed,
+                reset_token=None
+            )
+            return None
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Creates a hash password"""
