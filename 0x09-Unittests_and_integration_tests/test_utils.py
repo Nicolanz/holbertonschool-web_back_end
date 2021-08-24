c1f29996a0d2278ccc22e07c97209ba63f80517c
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-24 22:14:32 +0000

    mock

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Module containing the test cases for the utils.py module"""
 
-from utils import access_nested_map
 import unittest
+import requests
+from unittest import mock
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 from typing import (
     Tuple,
@@ -29,3 +31,19 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, a: Dict, b: Tuple, out: Any):
         """Method to handle and test utils.access_nested_map raises"""
         self.assertRaises(out, access_nested_map, a, b)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class to test the utils.get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @mock.patch('requests.get')
+    def test_get_json(self, test_url: str, test_payload: Dict,
+                      mock_requests: mock.MagicMock):
+        """Method to test get_json function"""
+        mock_requests(test_url)
+        mock_requests.return_value = test_payload
+        mock_requests.assert_called_once()
+        self.assertEqual(mock_requests(), get_json(test_url))
