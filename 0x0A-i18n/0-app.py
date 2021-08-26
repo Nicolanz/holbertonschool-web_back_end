d887006cdf7902e0b5abaa221c4611c1c82bb652
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-26 12:30:07 -0500

    0-app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,12 +1,13 @@
-#!/usr/local/bin/python3
-"""#!/usr/bin/env python3"""
-
+#!/usr/bin/env python3
+"""Setup a basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def render_index():
+    """Function to render to 0-index"""
     return render_template('0-index.html')
 
 
