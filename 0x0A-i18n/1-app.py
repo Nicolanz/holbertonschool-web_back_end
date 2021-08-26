a2dfcbded8f3550b0c37644210afba173d5f5ce4
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-26 14:01:30 -0500

    Config app

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/python3
+"""#!/usr/bin/env python3"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
-app = Flask(__name__)
 
 
 class Config():
@@ -10,6 +10,8 @@ class Config():
     LANGUAGES = ["en", "fr"]
 
 
+app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(
     app=app,
     default_locale=Config.LANGUAGES[0],
