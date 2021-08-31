968e173c7046eb0a79d554bd5b96d6570ff23b0e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-08-31 22:08:56 +0000

    idk

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Exercise Module"""
 
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 import redis
 
 
@@ -20,3 +20,13 @@ class Cache:
         key = str(key)
         self._redis.mset({key: data})
         return key
+
+        """Gets an element from the Redis hash table"""
+    def get(self, key: str, fn: Callable):
+        ele = self._redis.get(key)
+        try:
+            val = fn(ele)
+            return val
+        except Exception:
+            pass
+        return ele
