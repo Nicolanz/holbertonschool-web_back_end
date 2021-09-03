24ba76bbf5ac342eab9fcb1161e7ecbf5740df08
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-03 00:38:57 +0000

    Avg score

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,11 @@
+-- Procedure to set the avg score of a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER |
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+  BEGIN
+    SET @score = (SELECT ROUND(AVG(c.score)) FROM corrections c WHERE c.user_id = user_id GROUP BY c.user_id);
+    SELECT @score;
+    UPDATE users SET average_score = @score WHERE id = user_id;
+  END;
+|
+DELIMITER ;
