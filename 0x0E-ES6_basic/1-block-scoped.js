ba3b0a52516c8da55d2a7890c6a7c84235ff82af
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-09 19:26:14 +0000

    bScope

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -3,8 +3,16 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = function task() {
+      const task = true;
+      return task;
+    };
+    const task2 = function task2() {
+      const task2 = false;
+      return task2;
+    };
+    task();
+    task2();
   }
 
   return [task, task2];
