32bd30bba106888fa2e8ce646d3553e4286ab399
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-15 17:48:15 +0000

    Tasks 0 & 1

---
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const myArray = [];
+  const obj1 = new ClassRoom(19);
+  const obj2 = new ClassRoom(20);
+  const obj3 = new ClassRoom(34);
+
+  myArray.push(obj1, obj2, obj3);
+  return myArray;
+}
