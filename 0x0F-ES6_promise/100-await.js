60781e9e061e6a48ec77782c0c251085660ce290
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-15 05:26:00 +0000

    await

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from './utils';
+export default async function asyncUploadUser() {
+  const photo = uploadPhoto();
+  const user = createUser();
+  
+  return {}
+}
