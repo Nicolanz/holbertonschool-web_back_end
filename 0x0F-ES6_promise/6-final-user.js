e1faa8d4d80a086490931e3a3bbe551a9b66be36
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-15 02:05:49 +0000

    All Settled

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUser = signUpUser(firstName, lastName);
+  const uploadPicture = uploadPhoto(fileName);
+  const myArray = [];
+
+  return Promise
+    .allSettled([signUser, uploadPicture]).then((values) => {
+      for (const i of values) {
+        myArray.push({
+          status: i.status,
+          value: i.value,
+        });
+      }
+      return myArray;
+    });
+}
