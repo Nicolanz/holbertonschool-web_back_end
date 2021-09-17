9a4494fe1f445b5f6de28b9796d6e0f8b3e44bf2
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-17 21:07:05 +0000

    All

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Module to list all documents in python"""
 
-from pymongo import MongoClient
 
 def list_all(mongo_collection):
     """Function to list the documents of a collection"""
-    client 
-
+    my_list = []
+    elements = mongo_collection.find({})
+    for i in elements:
+        my_list.append(i)
+    return my_list
