2ff30e349be20005e011800d1fc723feb4493550
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-21 21:56:49 +0000

    ser

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   let myStr = '';
-  if (!startString) {
+  if (!startString || startString.length === 0 || typeof startString !== 'string') {
     return myStr;
   }
 
