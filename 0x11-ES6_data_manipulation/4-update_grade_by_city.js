94d6eec2fcb1b802bc493c28cac00b81f3173081
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-21 02:29:53 +0000

    Mapping into map

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  const newList = [];
+  getListStudents.filter((obj) => obj.location === city).map((x) => {
+    const newObj = x;
+    newGrades.map((i) => {
+      if (newObj.id === i.studentId) {
+        newObj.grade = i.grade;
+        newList.push(newObj);
+      } else if (newGrades.at(-1) === i && newObj.grade === undefined) {
+        newObj.grade = 'N/A';
+        newList.push(newObj);
+      }
+      return i.studentId;
+    });
+    return newObj;
+  });
+  return newList;
+}
