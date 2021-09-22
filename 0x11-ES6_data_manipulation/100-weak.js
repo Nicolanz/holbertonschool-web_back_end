255412767bc1d8209f2f079a5c55479c011df892
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-22 04:06:25 +0000

    weak

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,13 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
-  if (weakMap.get(endpoint) >= 5) {
-    throw new Error('Endpoint load is high');
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, count + 1);
   }
-  weakMap.set(endpoint, count + 1);
 }
