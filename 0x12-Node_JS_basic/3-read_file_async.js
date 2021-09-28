35a17f0c21db98d21b50009873f857a304a87cd2
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-28 02:10:25 +0000

    read file

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -30,12 +30,14 @@ function countStudents(database) {
         }
 
         const keys = Object.keys(newObj);
+        let str = '';
         console.log(`Number of students: ${counter}`);
 
         for (let i = 0; i < keys.length; i += 1) {
+          str += `Number of students in ${keys[i]}: ${newObj[keys[i]].count}. List: ${newObj[keys[i]].names.join(', ')}\n`;
           console.log(`Number of students in ${keys[i]}: ${newObj[keys[i]].count}. List: ${newObj[keys[i]].names.join(', ')}`);
         }
-        resolve();
+        resolve(str);
       }
     });
   });
