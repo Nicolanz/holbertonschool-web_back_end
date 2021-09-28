923db8112b3e8a88d5b7308d133962785dba6248
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-28 02:39:33 +0000

    5-http

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2]).then((value) => {
+      res.end(`This is the list of our students\n${value}`);
+    }).catch((err) => {
+      res.end(err.message);
+    });
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log('Welcome!');
+});
+
+module.exports = app;
