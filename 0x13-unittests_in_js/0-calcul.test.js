44c5934b56b14db2f7d022977425bdb34e7ddcb3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-29 20:03:53 +0000

    0-tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,23 @@
-
 const assert = require('assert');
-const calculateNumber = require("./0-calcul.js");
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
+describe('calculateNumber', () => {
   it('should return an int number', () => {
-      assert.equal(typeof calculateNumber(2.3, 3.1), 'number', 'result is not rounded');
+    assert.equal(typeof calculateNumber(2.3, 3.1), 'number');
   });
-  it('should return sum of nums', () => {
-      assert.equal(calculateNumber(2.2, 2,3), 4, "Not expected result");
+  it('should return sum of num', () => {
+    assert.equal(calculateNumber(2.2, 2, 3), 4);
   });
-  it('should return sum of nums', () => {
-      assert.equal(calculateNumber(1, 3), 4, "Not expected result");
+  it('should return sum', () => {
+    assert.equal(calculateNumber(1, 3), 4);
   });
-  it('should return sum of nums', () => {
-      assert.equal(calculateNumber(1, 3.7), 5, "Not expected result");
+  it('sum of nums', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('should return sum of nums', () => {
-      assert.equal(calculateNumber(1.2, 3.7), 5, "Not expected result");
+  it('return sum of nums', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return sum of nums', () => {
-      assert.equal(calculateNumber(1.5, 3.7), 6, "Not expected result");
+  it('sum', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
