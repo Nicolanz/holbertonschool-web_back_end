4fd82c91aba6397c91d8674759dad73cc7eceb40
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-29 00:51:19 +0000

    utils

--- 0x12-Node_JS_basic/full_server/utils.js
+++ 0x12-Node_JS_basic/full_server/utils.js
@@ -6,8 +6,22 @@ function readDatabase(filePath) {
       if (err) {
         reject(err);
       } else {
-        const newArray = { data };
-        resolve(newArray);
+        const newObj = {};
+        const newData = data.split('\n');
+
+        for (let i = 0; i < newData.length; i += 1) {
+          newData[i] = newData[i].split(',');
+        }
+        for (let i = 1; i < newData.length; i += 1) {
+          if (newData[i].length > 1) {
+            if (!Object.prototype.hasOwnProperty.call(newObj, newData[i][newData[i].length - 1])) {
+              newObj[newData[i][newData[i].length - 1]] = [newData[i][0]];
+            } else {
+              newObj[newData[i][newData[i].length - 1]].push(newData[i][0]);
+            }
+          }
+        }
+        resolve(newObj);
       }
     });
   });
