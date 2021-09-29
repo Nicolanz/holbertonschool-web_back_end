1e9a97a95a9fb5f188dff44818e8259ba02436de
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-29 19:47:23 +0000

    More tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,6 +4,21 @@ const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', function() {
   it('should return an int number', () => {
-      assert.equal(typeof calculateNumber(2.3, 3.1), 'number');
-  })
+      assert.equal(typeof calculateNumber(2.3, 3.1), 'number', 'result is not rounded');
+  });
+  it('should return sum of nums', () => {
+      assert.equal(calculateNumber(2.2, 2,3), 4, "Not expected result");
+  });
+  it('should return sum of nums', () => {
+      assert.equal(calculateNumber(1, 3), 4, "Not expected result");
+  });
+  it('should return sum of nums', () => {
+      assert.equal(calculateNumber(1, 3.7), 5, "Not expected result");
+  });
+  it('should return sum of nums', () => {
+      assert.equal(calculateNumber(1.2, 3.7), 5, "Not expected result");
+  });
+  it('should return sum of nums', () => {
+      assert.equal(calculateNumber(1.5, 3.7), 6, "Not expected result");
+  });
 });
