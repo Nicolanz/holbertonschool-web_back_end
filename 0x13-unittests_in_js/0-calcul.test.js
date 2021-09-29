6e8b333bf1c0ae7a06a20d67caf5c3512c87e7e5
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-09-29 20:27:26 +0000

    idk

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -11,5 +11,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(-2, -2), -4);
     assert.strictEqual(calculateNumber(-1.3, -4.5), -5);
+    assert.strictEqual(calculateNumber(0.4, 0.6), 1);
+    assert.strictEqual(calculateNumber(0.4, 0.2), 0);
   });
 });
